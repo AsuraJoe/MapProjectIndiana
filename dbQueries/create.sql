33636d454b1c430367fcfb83925c2181f80975d0
AsuraJoe <41365507+AsuraJoe@users.noreply.github.com>
2020-11-03 18:47:03 -0500

    add sql scripts for tables

---
+++ dbQueries/create.sql
@@ -0,0 +1,21 @@
+DROP TABLE IF EXISTS provinces;
+CREATE TABLE provinces(
+    provinceID INT NOT NULL,
+    ProvinceName VARCHAR(50),
+    PRIMARY KEY(provinceID)
+);
+
+DROP TABLE IF EXISTS pci_data;
+CREATE TABLE pci_data(
+    data_year INT NOT NULL,
+    province INT NOT NULL REFERENCES provinces(provinceID) ,
+    transperency FLOAT(5,5),
+    time_costs FLOAT(5,5),
+    informal_charges FLOAT(5,5),
+    bias FLOAT(5,5),
+    proactivity FLOAT(5,5),
+    bss FLOAT(5,5),
+    labor_training FLOAT(5,5),
+    law_order FLOAT(5,5),
+    CONSTRAINT province_year PRIMARY KEY(data_year, province)
+);
