3503e1a1e2c0b67519c5ae0c58a23b70ed9b65bb
AsuraJoe <41365507+AsuraJoe@users.noreply.github.com>
2020-11-11 17:44:11 -0500

    Add README

---
+++ dbQueries/insertPCI2019.sql
@@ -0,0 +1,143 @@
+insert into pci_data (province, ranking, pci, transparency, time_costs, 
+informal_charges, bias, proactivity, bss, labor_training, law_order) 
+Values(1,)
+
+insert into pci_data (province, ranking, pci, transparency, time_costs, 
+informal_charges, bias, proactivity, bss, labor_training, law_order) 
+Values(2,)
+
+insert into pci_data (province, ranking, pci, transparency, time_costs, 
+informal_charges, bias, proactivity, bss, labor_training, law_order) 
+Values(3,)
+
+insert into pci_data (province, ranking, pci, transparency, time_costs, 
+informal_charges, bias, proactivity, bss, labor_training, law_order) 
+Values(4,)
+
+insert into pci_data (province, ranking, pci, transparency, time_costs, 
+informal_charges, bias, proactivity, bss, labor_training, law_order) 
+Values(5,)
+
+insert into pci_data (province, ranking, pci, transparency, time_costs, 
+informal_charges, bias, proactivity, bss, labor_training, law_order) 
+Values(6,)
+
+insert into pci_data (province, ranking, pci, transparency, time_costs, 
+informal_charges, bias, proactivity, bss, labor_training, law_order) 
+Values(7,)
+
+insert into pci_data (province, ranking, pci, transparency, time_costs, 
+informal_charges, bias, proactivity, bss, labor_training, law_order) 
+Values(8,)
+
+insert into pci_data (province, ranking, pci, transparency, time_costs, 
+informal_charges, bias, proactivity, bss, labor_training, law_order) 
+Values(9,)
+
+insert into pci_data (province, ranking, pci, transparency, time_costs, 
+informal_charges, bias, proactivity, bss, labor_training, law_order) 
+Values(10,)
+
+insert into pci_data (province, ranking, pci, transparency, time_costs, 
+informal_charges, bias, proactivity, bss, labor_training, law_order) 
+Values(11,)
+
+insert into pci_data (province, ranking, pci, transparency, time_costs, 
+informal_charges, bias, proactivity, bss, labor_training, law_order) 
+Values(12,)
+
+insert into pci_data (province, ranking, pci, transparency, time_costs, 
+informal_charges, bias, proactivity, bss, labor_training, law_order) 
+Values(13,)
+
+insert into pci_data (province, ranking, pci, transparency, time_costs, 
+informal_charges, bias, proactivity, bss, labor_training, law_order) 
+Values(14,)
+
+insert into pci_data (province, ranking, pci, transparency, time_costs, 
+informal_charges, bias, proactivity, bss, labor_training, law_order) 
+Values(15,)
+
+insert into pci_data (province, ranking, pci, transparency, time_costs, 
+informal_charges, bias, proactivity, bss, labor_training, law_order) 
+Values(16,)
+
+insert into pci_data (province, ranking, pci, transparency, time_costs, 
+informal_charges, bias, proactivity, bss, labor_training, law_order) 
+Values(17,)
+
+insert into pci_data (province, ranking, pci, transparency, time_costs, 
+informal_charges, bias, proactivity, bss, labor_training, law_order) 
+Values(18,)
+
+insert into pci_data (province, ranking, pci, transparency, time_costs, 
+informal_charges, bias, proactivity, bss, labor_training, law_order) 
+Values(19,)
+
+insert into pci_data (province, ranking, pci, transparency, time_costs, 
+informal_charges, bias, proactivity, bss, labor_training, law_order) 
+Values(20,)
+
+insert into pci_data (province, ranking, pci, transparency, time_costs, 
+informal_charges, bias, proactivity, bss, labor_training, law_order) 
+Values(21,)
+
+insert into pci_data (province, ranking, pci, transparency, time_costs, 
+informal_charges, bias, proactivity, bss, labor_training, law_order) 
+Values(22,)
+
+insert into pci_data (province, ranking, pci, transparency, time_costs, 
+informal_charges, bias, proactivity, bss, labor_training, law_order) 
+Values(23,)
+
+insert into pci_data (province, ranking, pci, transparency, time_costs, 
+informal_charges, bias, proactivity, bss, labor_training, law_order) 
+Values(24,)
+
+insert into pci_data (province, ranking, pci, transparency, time_costs, 
+informal_charges, bias, proactivity, bss, labor_training, law_order) 
+Values(1,)
+
+insert into pci_data (province, ranking, pci, transparency, time_costs, 
+informal_charges, bias, proactivity, bss, labor_training, law_order) 
+Values(1,)
+
+insert into pci_data (province, ranking, pci, transparency, time_costs, 
+informal_charges, bias, proactivity, bss, labor_training, law_order) 
+Values(1,)
+
+insert into pci_data (province, ranking, pci, transparency, time_costs, 
+informal_charges, bias, proactivity, bss, labor_training, law_order) 
+Values(1,)
+
+insert into pci_data (province, ranking, pci, transparency, time_costs, 
+informal_charges, bias, proactivity, bss, labor_training, law_order) 
+Values(1,)
+
+insert into pci_data (province, ranking, pci, transparency, time_costs, 
+informal_charges, bias, proactivity, bss, labor_training, law_order) 
+Values(1,)
+
+insert into pci_data (province, ranking, pci, transparency, time_costs, 
+informal_charges, bias, proactivity, bss, labor_training, law_order) 
+Values(1,)
+
+insert into pci_data (province, ranking, pci, transparency, time_costs, 
+informal_charges, bias, proactivity, bss, labor_training, law_order) 
+Values(1,)
+
+insert into pci_data (province, ranking, pci, transparency, time_costs, 
+informal_charges, bias, proactivity, bss, labor_training, law_order) 
+Values(1,)
+
+insert into pci_data (province, ranking, pci, transparency, time_costs, 
+informal_charges, bias, proactivity, bss, labor_training, law_order) 
+Values(1,)
+
+insert into pci_data (province, ranking, pci, transparency, time_costs, 
+informal_charges, bias, proactivity, bss, labor_training, law_order) 
+Values(1,)
+
+insert into pci_data (province, ranking, pci, transparency, time_costs, 
+informal_charges, bias, proactivity, bss, labor_training, law_order) 
+Values(1,)
